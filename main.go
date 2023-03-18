afea647027cf0832cc2df32f16057f17adea2468
Sddilora <sumeyyedilaradogan@gmail.com>
2023-03-18 16:59:08 +0300

    project's functions called in main

--- main.go
+++ main.go
@@ -1,9 +1,5 @@
 package main
 
-import (
-	"lesson/project"
-)
-
 func main() {
 
 	// interfaces.Demo1()
@@ -30,11 +26,42 @@ func main() {
 	//project.GetAllProducts()
 
 	// project.AddProduct()
-	// project.GetAllProducts()
+	// project.GetAllProducts() // To see if wee succeed
 
 	// project.AddProduct2()
-	// project.GetAllProducts()
+	// project.GetAllProducts() //To see if wee succeed
 
-	project.AddProduct3()
-	project.GetAllProducts()
+	// project.AddProduct3()
+	// project.GetAllProducts() //To see if wee succeed
+
+	// products, err := project.GetAllProducts2()
+	// log.Println(products, err)
+
+	// products, _ := project.GetAllProducts2()
+	// for i := 0; i < len(products); i++ {
+	// 	fmt.Println(products[i].ProductName)
+	// }
+	// //Output :
+	// // Laptop
+	// // Mouse
+	// // Water
+	// // Cell Phone
+	// // Microphone
+	// // Microphone
+
+	// project.AddProduct_2()
+	// products, _ := project.GetAllProducts2()
+	// for i := 0; i < len(products); i++ {
+	// 	fmt.Println(products[i].ProductName)
+	// }
+
+	//Output
+	//
+	// Laptop
+	// Mouse
+	// Water
+	// Cell Phone
+	// Microphone
+	// Microphone
+	// Keyboard
 }
